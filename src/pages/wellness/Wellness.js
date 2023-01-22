17527ca329edb6802fa8207a7cdd0af073cbe46d
LIamB12 <liam.beenken@gmail.com>
2023-01-22 09:54:11 -0500

    final

--- src/pages/wellness/Wellness.js
+++ src/pages/wellness/Wellness.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./wellness.css"
 const qs = [
-    "How was your day?:",
+    "How was your day?",
     "How was your workload?:",
-    "Do you feel like you had support you needed today?:",
-    "Did you have a good rest before the workday?:",
-    "Please rate your degree of work-related burnout?:",
-    "Do you feel like you have a optimal work-life balance?:",
-    "How well would you say our organization supports employees in achieving a better balance between work and other responsibilities?:",
+    "Do you feel like you had support you needed today?",
+    "Did you have a good rest before the workday?",
+    "Please rate your degree of work-related burnout?",
+    "Do you feel like you have a optimal work-life balance?",
+    "How well would you say our organization supports employees in achieving a better balance between work and other responsibilities?",
 ]
 const Wellness = () => {
     
@@ -15,6 +15,7 @@ const Wellness = () => {
 
   return (
     <div className="survey_cont">
+        <h1>Please rate each of the following on a scale from 0-5</h1>
         <form>
         {qs.map((c) => {
             return(
@@ -24,6 +25,8 @@ const Wellness = () => {
             </div>
             )
         })}
+        <br></br>
+        <br></br>
         <button type="submit">Submit</button>
         </form>
     </div>
